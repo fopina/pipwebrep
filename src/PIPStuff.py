163cbdf1f5acd02c4b9c6a2a530d5aae2d2a94c4
Filipe Pina <fopina@skmobi.com>
2014-03-10 22:36:04 +0100

    initial working version

---
+++ src/PIPStuff.py
@@ -0,0 +1,34 @@
+from java.sql import DriverManager,Statement,ResultSet,ResultSetMetaData
+
+# stupid bug...
+# make sure any module that uses PIPStuff calls Class.forName
+# import java.lang.Class
+# java.lang.Class.forName(DRIVER_NAME)
+
+class PIPStuff():
+	def __init__(self, dburl, dbuser, dbpass):
+		self._dburl = dburl
+		self._dbuser = dbuser
+		self._dbpass = dbpass
+	
+	def connect(self):
+		return DriverManager.getConnection(self._dburl,self._dbuser,self._dbpass)
+
+	def executeSQL(self, sqlQry):
+		connection = self.connect()
+		statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+		rs = statement.executeQuery(sqlQry)
+		rsmd = rs.getMetaData()
+
+		headers, rows = ([],[])
+
+		for i in xrange(1, rsmd.getColumnCount()):
+			headers.append(rsmd.getColumnLabel(i))
+
+		while rs.next():
+			cols = []
+			for i in xrange(1, rsmd.getColumnCount()):
+				cols.append(rs.getString(i))
+			rows.append(cols)
+
+		return (headers,rows)
---
+++ src/jywebrep.py
@@ -0,0 +1,85 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+from functools import wraps
+from PIPStuff import PIPStuff
+import settings
+
+import java.lang.Class
+java.lang.Class.forName(settings.DRIVER)
+
+app = Flask(__name__)
+
+# dirty fix for app root_path because module is loaded as "__builtin__" (in Tomcat)
+# as flask is in the app WEB-INF
+# sys.modules['flask'].__file__
+# is something like
+# /usr/tomcat6/webapps/jyWebRep/WEB-INF/lib-python/flask/__init__.py
+import sys,os
+app.root_path = os.path.abspath(os.path.dirname(sys.modules['flask'].__file__)+'/../../../')
+
+def requires_login():
+	def wrapper(f):
+		@wraps(f)
+		def wrapped(*args, **kwargs):
+			if 'profile' not in session:
+				return redirect(url_for('login'))
+			return f(*args, **kwargs)
+		return wrapped
+	return wrapper
+
+@app.route('/')
+@requires_login()
+def index():
+	return render_template('main.html', name = 'asd')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		profile = PIPStuff(settings.URL, request.form['username'], request.form['password'])
+		try:
+			connection = profile.connect()
+		except:
+			print 'invalid login'
+			return redirect(url_for('login'))
+		
+		session['profile'] = profile
+		return redirect(url_for('index'))
+	return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('profile', None)
+	return redirect(url_for('index'))
+
+@app.route('/query', methods=['GET', 'POST'])
+@requires_login()
+def query():
+	if request.method != 'POST':
+		return redirect(url_for('index'))
+	if 'query' not in request.form:
+		return redirect(url_for('index'))
+
+	profile = session['profile']
+	stmt = request.form['query']
+
+	try:
+		headers, results = profile.executeSQL(stmt)
+	except:
+		print 'error with query'
+		return redirect(url_for('query'))
+
+	return render_template('query.html', query = stmt, headers = headers, results = results)
+
+@app.errorhandler(500)
+def internal_error(error):
+	return repr(error)
+
+@app.errorhandler(404)
+def not_found(error):
+	return repr(error)
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?R1'
+
+
+if __name__ == '__main__':
+	app.run()

7a6b729ac9e0123157f240595cbed770cdd1698b
Filipe Pina <fopina@skmobi.com>
2014-03-16 01:54:47 +0100

    pipstuff test

--- src/jywebrep_test.py
+++ src/jywebrep_test.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 import jywebrep
+import flask
 import unittest
 
 TESTID = '1'
@@ -29,7 +30,21 @@ class jyWebRepTestCase(unittest.TestCase):
 		assert '<form method="POST" action="login"' in rv.data
 
 	def test_pipstuff_sql(self):
-		pass
+		with self.app as c:
+			rv = c.post('/login', data = dict(
+				username = TESTID,
+				password = TESTPWD
+			), follow_redirects=True)
+			profile = flask.session.get('profile')
+			assert profile
+			rows = profile.execute_query('SELECT CONAM from CUVAR')
+			oldv = rows[0][0]
+			newv = '123'
+			profile.execute_update("UPDATE CUVAR SET CONAM=?", [ newv ])
+			assert profile.execute_query('SELECT CONAM from CUVAR')[0][0] == newv
+			profile.execute_update("UPDATE CUVAR SET CONAM=?", [ oldv ])
+			assert profile.execute_query('SELECT CONAM from CUVAR')[0][0] == oldv
+		
 
 	def login(self, username, password):
 		return self.app.post('/login', data=dict(
